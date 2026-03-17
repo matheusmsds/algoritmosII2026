30765b50c2219903d0dff4d6af41b649a050f4d9
matheusmsds <matheus.mendes.soaresdasilva@gmail.com>
2026-03-17 12:32:39 -0300

    Update InverterNumeros.java

--- java-exercicios/InverterNumeros.java
+++ java-exercicios/InverterNumeros.java
@@ -3,9 +3,15 @@ import javax.swing.JOptionPane;
 public class InverterNumeros{
 	public static void main(String args[]){
 		int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o numero"));
-		for(int i = 1; i <= numero; i--){
-			System.out.println(numero);
+		int reverso = 0;
+		
+		while(numero > 0){
+			int digito = numero % 10;
+			reverso = (reverso * 10) + digito;
+			numero = numero / 10;
 		}
+		
+		System.out.println(reverso);
+		
 	}
-
 }
