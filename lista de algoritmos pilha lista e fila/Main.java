319e5f6212d54ae7e99aadad8bcb0ffa8aca8cfe
Matheus Mendes <matheus.mendes.soaresdasilva@gmail.com>
2026-08-26 14:02:04 -0300

    add lista de exercicio pilha, fila e lista

--- lista de algoritmos pilha lista e fila/Main.java
+++ lista de algoritmos pilha lista e fila/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        
+        armazenarFila();
+        armazenarPilha();
     }
+
+    public static void armazenarPilha(){
+        Pilha pilha = new Pilha();
+
+        for(int i = 0; i < 5; i++){
+            pilha.push(i+1);
+        }
+
+        System.out.println();
+
+        pilha.print();
+
+    }
+
+    public static void armazenarFila(){
+        Fila fila = new Fila();
+
+        for(int i = 0; i < 5; i++){
+            fila.push(i+1);
+        }
+
+        System.out.println();
+
+        fila.print();
+
+    }
+
+
 }
